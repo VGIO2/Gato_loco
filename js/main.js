7e8b7425c9794977df0f7353c9fa4c1303843f22
Giovanna <orurog@gmail.com>
2017-03-14 14:51:09 -0500

    ajax/modificacion html

--- js/main.js
+++ js/main.js
@@ -19,7 +19,9 @@ $('#btn-nombres').on('click', function(){
                 player = 1;
             }
         }
-    })
+    });
+    
+    
 });
 
 
@@ -30,11 +32,21 @@ function init()
 	currentSection = $('#saludo');
 	$('#btn-saludo').click(onClickBtnSaludo);
 	$('#btn-nombres').click(onClickBtnNombre);
+	$('#btn-historial').click(onClickBtnHistorial);
+    $('#lista-juegos').on('click', 'button', onClickBtnItemJuego);
 
 	TweenMax.from($('#saludo h1'), 1, {marginBottom:'0px', ease:Elastic.easeOut});
 }
  
-
+function onClickBtnItemJuego()
+{
+	var idGame = $(this).parent().data('idgame');
+	console.log(idGame);
+    
+    
+    gotoSection
+	//getSingleGame(idGame);
+}
 function onClickBtnSaludo() {
 	gotoSection('nombres');
 }
@@ -42,9 +54,14 @@ function onClickBtnSaludo() {
 function onClickBtnNombre() {
 	gotoSection('juego');
 }
+function onClickBtnHistorial(evt) {
+	evt.preventDefault();
+	gotoSection('historial');
+	getHistorial();
+}
 
-function gotoSection(_identificadorDeSeccion)
-{
+function gotoSection(_identificadorDeSeccion){
+    
 	currentSection.removeClass('visible');
 	var nextSection = $('#'+_identificadorDeSeccion);
 
@@ -53,4 +70,57 @@ function gotoSection(_identificadorDeSeccion)
 	TweenMax.from(nextSection, 1.5, {scale:0.2, opacity:0, ease:Elastic.easeOut});
 	currentSection = nextSection;
 }
+
+function getHistorial() {
+	$.ajax({
+		url: 'http://test-ta.herokuapp.com/games'
+	}).success(function (_data) {
+		dibujarHistorial(_data);
+	});
+}
+
+
+function getSingleGame(_idGame)
+{
+	$.ajax({
+		url: 'http://test-ta.herokuapp.com/games/' + _idGame,
+		type:'GET'
+	}).success(function(_data
+    ){
+		console.log(_data);
+	});
+}
+
+function dibujarHistorial(_datos) {
+	//console.log(_datos);
+	var lista = $('#lista-juegos');
+
+	for (var i in _datos) {
+		console.log(_datos[i].winner_player);
+
+		var html = '<li data-idgame="'+ _datos[i].id +'" class="list-group-item"><button class="btn">Ver</button> Ganador: ' + _datos[i].winner_player + '</li>';
+		lista.append(html);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
